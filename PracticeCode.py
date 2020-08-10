f471a16a20872612ed94bdfd5857d354dc488047
skandsrivastava <65039596+skandsrivastava@users.noreply.github.com>
2020-08-10 13:57:56 +0530

    Add files via upload

---
+++ PracticeCode.py
@@ -0,0 +1,84 @@
+#print("Skand")
+
+###########################
+
+#def add_number(num1, num2):
+  #return num1 + num2
+
+#result = add_number(2,8)
+
+#print(result)
+
+#############################
+
+"""A= int ( input("Enter The Basic Salary :"))
+B= int ( input( "Enter The Amount Of Saving Done During The Month :"))
+
+if A <= 50000:
+  TT =0
+
+elif A <= 80000:
+  if B <= 50000:
+    TS = A - B
+    TT = TS * 0.05
+
+elif A <= 100000:
+  if B <= 70000:
+    TS = A - B
+    TT = TS * 0.10
+
+else:
+
+  if B <= 100000:
+    TS = A - B
+    TT = TS * 0.19
+
+print ("THE AMOUNT LEFT AFTER SAVING IS :",TS) 
+print ("THE TAX YOU HAVE TO PAY AT LAST IS :",TT)
+print ("$")
+print ("$")
+print ("YOU ARE A RESPONSIBLE CITIZEN OF INDIAN")"""
+
+#################################
+
+## FOR LOOP PROGRAM:::::::::::::
+
+#A = range (10)
+#print (A)
+
+#for i in range (10):
+ # print (i)
+
+#for i in range (100000):
+ # print (i**3)
+
+
+
+
+#WAP to generate the table of a no ask by a user?
+"""Table = int ( input ("ENTER A NO WHICH TABLE YOU WANT :")) 
+for i in range (1,11) :
+  print(Table,"X",i,"=",Table*i)"""
+
+
+
+
+#WAP to print cubes of number in a range?
+"""a = int ( input("ENTER THE FIRST NO OF THE RANGE FOR WHICH YOU WANT A CUBE OF A NUMBER: "))
+b = int ( input("ENTER THE SECOND NO OF THE RANGE FOR WHICH YOU WANT A CUBE OF A NUMBER: "))
+c = b + 1
+for i in range (a , c):
+  print ("CUBES OF A NUMBER'S",i,"is:",i**3)"""
+
+
+
+#WAP to print a square ROOT of every alternate number in a range?
+"""a = int ( input("ENTER THE FIRST NO OF THE RANGE FOR WHICH YOU WANT A SQUARE ROOT OF AN ALTERNATE NUMBER: "))
+b = int ( input("ENTER THE SECOND NO OF THE RANGE FOR WHICH YOU WANT A SQUARE ROOT OF AN ALTERNATE NUMBER: "))
+c = b + 1 
+for i in range (a , c , 2):
+  print ("SQUARE OF",i,"is :",i**0.5)"""
+
+
+
+########  WHILE LOOP PPROGRAM :::::::::::::::::::
